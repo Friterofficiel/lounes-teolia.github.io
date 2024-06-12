aa2a74d2f75268f381af5bfe77033e28da7369e4
zenadj_l <zenadj_l@etna-alternance.net>
2024-06-12 21:47:53 +0200

    modif

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
 // src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/Home/home';
-import LoginPage from './pages/Login/login';
-import RegisterPage from './pages/Register/register';
-import DashboardPage from './pages/Dashboard/dashboard';
-import StudentsPage from './pages/Students/students';
-import ProfilPage from './pages/Profile/profile';
-import AboutPage from './pages/About/about';
+import HeaderComp from './components/HeaderComp';
+import StudentsComp from './components/StudentsComp';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
 import PrivateRoute from './components/PrivateRoute';
-
+import AboutComp from './components/AboutComp';
+import ProfilePage from './components/ProfilePage';
 
 
 const App: React.FC = () => {
   return (
     <Router>
-      <HomePage />
+      <HeaderComp />
       <Routes>
-        <Route path="/lounes-teolia.github.io/login" element={<LoginPage />} />
-        <Route path="/lounes-teolia.github.io/register" element={<RegisterPage />} />
-        <Route path="/lounes-teolia.github.io/about" element={<AboutPage />} />
-        <Route path="/lounes-teolia.github.io/profile" element={<ProfilPage />} />
+        <Route path="/lounes-teolia.github.io/login" element={<Login />} />
+        <Route path="/lounes-teolia.github.io/register" element={<Register />} />
+        <Route path="/lounes-teolia.github.io/about" element={<AboutComp />} />
+        <Route path="/lounes-teolia.github.io/profile" element={<ProfilePage />} />
         <Route element={<PrivateRoute />}>
-          <Route path="/lounes-teolia.github.io/students" element={<StudentsPage />} />
-          <Route path="/lounes-teolia.github.io/dashboard" element={<DashboardPage />} />
+          <Route path="/lounes-teolia.github.io/students" element={<StudentsComp />} />
+          <Route path="/lounes-teolia.github.io/dashboard" element={<Dashboard />} />
         </Route>
         <Route path="*" element={<div></div>} />
       </Routes>
